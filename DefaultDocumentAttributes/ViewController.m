738a5fa4761160efa227a5d4d6b2790a195b3d72
Dave Verwer <dave.verwer@shinydevelopment.com>
2013-12-02 13:42:01 +0000

    Updated to work how the documentation says it should work. Unfortunately it still doesn't work.

--- DefaultDocumentAttributes/ViewController.m
+++ DefaultDocumentAttributes/ViewController.m
@@ -6,13 +6,17 @@
 {
   [super viewDidLoad];
 
-  NSString *HTMLString = @"<p>Hello, <strong>World</strong>.";
+  NSString *HTMLString = @"<p>Hello, <strong>World</strong>.</p>";
   NSData *HTMLData = [HTMLString dataUsingEncoding:NSUTF8StringEncoding];
 
   NSError *attributedStringError = nil;
+  NSDictionary *documentReturnedAttributes = nil;
   NSDictionary *documentAttributes = @{ NSDefaultAttributesDocumentAttribute : @{ NSFontAttributeName : [UIFont fontWithName:@"MarkerFelt-Wide" size:30], NSForegroundColorAttributeName : [UIColor redColor] } };
-  NSAttributedString *attributedString = [[NSAttributedString alloc] initWithData:HTMLData options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:&documentAttributes error:&attributedStringError];
+  NSAttributedString *attributedString = [[NSAttributedString alloc] initWithData:HTMLData options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSDefaultAttributesDocumentAttribute : documentAttributes } documentAttributes:&documentReturnedAttributes error:&attributedStringError];
   if (attributedString == nil) NSLog(@"%@", attributedStringError);
+
+  // Note: No font or colour attributes in this returned attributes array
+  NSLog(@"Returned attributes: %@", documentReturnedAttributes);
 
   self.label.attributedText = attributedString;
 }
